b0907d41f8361ff0a27c173566ca989960fb4e33
Shelby Sash <shelbyksash@gmail.com>
2024-11-04 17:49:56 -0500

    Add project files.

---
+++ FriendlyGerbil_Assignment09/dbUtilitiesPackage/dbUtilities.py
@@ -0,0 +1,31 @@
+# Name(s): Shelby Sash, Sidney Huschart, Andrew Mehlman
+# email: sashsk@mail.uc.edu, 
+# Assignment Number: Assignment 09
+# Due Date: November 7,2024
+# Course #/Section: IS4010/ 001
+# Semester/Year: Fall 2024
+# Brief Description of the assignment: Collaborate using github to connect to a databse and extract results 
+# Brief Description of what this module does: Function for connecting to the SQL Server database 
+# Citations: N/A
+# Anything else that's relevant: N/A
+
+#db utilities
+
+import pyodbc
+
+def connect_to_database():
+    """
+    Connect to the database
+    @return Connecition Object: the open connection, or None on error
+    """
+    try:
+        conn = pyodbc.connect('Driver={SQL Server};'
+                              'Server=lcb-sql.uccob.uc.edu\\nicholdw;' 
+                              'Database=GroceryStoreSimulator;'
+                              'uid=IS4010Login;'
+                              'pwd=P@ssword2;')  
+    except: 
+        conn = None  #failture 
+        
+    return conn
+
---
+++ FriendlyGerbil_Assignment09/mainPackage/main.py
@@ -0,0 +1,34 @@
+# Name(s): Shelby Sash, Sidney Huschart, Andrew Mehlman
+# email: sashsk@mail.uc.edu
+# Assignment Number: Assignment 09
+# Due Date: November 7,2024
+# Course #/Section: IS4010/ 001
+# Semester/Year: Fall 2024
+# Brief Description of the assignment: Collaborate using github to connect to a databse and extract results
+# Brief Description of what this module does: Produces a randomized output of product descriptions 
+# Citations: stack overflow
+# Anything else that's relevant: N/A
+
+
+#main.py
+
+from dbUtilitiesPackage.dbUtilities import *
+
+import random  #for step 2
+
+import pyodbc
+
+
+try:
+    conn= connect_to_database() 
+    # Submit a query to the SQL Server instance and store the results in the cursor object
+    cursor = conn.cursor()  #establishes temporary table 
+    cursor.execute('SELECT * FROM tProduct') #submits sql to db engine and stores in cursor
+except Exception as e: 
+    #I'd like to know more
+    print("error accessing database")
+    print(e) 
+    exit()  #given up. how do i get out of this module 
+    
+
+
